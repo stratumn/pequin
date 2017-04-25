dc2551908b1a9c23308c67776ddc0ab4296148c7
Ankur Shah Delight <mangolandia@gmail.com>
2017-04-25 10:57:21 -0700

    we now autogenerate this aux files

--- pepper/proof_of_balance/proof_of_balance.c
+++ pepper/proof_of_balance/proof_of_balance.c
@@ -2,47 +2,66 @@
 
 //  Proof of balance
 struct In {
-  uint32_t encrypted_balance;
-  uint32_t balance_to_prove;
+  uint64_t encrypted_balance;
+  uint64_t balance_to_prove;
 };
 
 struct Out {
+  uint64_t decrypted_balance;
   int valid;
 };
 
 // a ^ b % c
-int powm(int a, int b, int c) {
-  long long res = 1;
+int powm(uint64_t a, uint64_t b, uint64_t c) {
+  uint64_t res = 1;
   a %= c;
   int i;
-  for (i=0; i<100; i++) {
-  	if (b > 0) {
-	    if (b % 2 == 1) {
-	      res = (res * a) % c;
-	    }
-	    b = b >> 1;
-	    a = (a * a) % c;
+  for (i=0; i<64; i++) {
+    if (b > 0) {
+      if (b % 2 == 1) {
+        res = (res * a) % c;
+      }
+      b = b >> 1;
+      a = (a * a) % c;
     }
   }
   return res;
 }
 
-int compute(struct In *input, struct Out *output) {
-	// get witness values (private to prover)
-  uint32_t *dummy_inputs[1] = {&(input->balance_to_prove)}; // this can be anything
-  uint32_t input_lens[1] = {0};
-  int private_key[2];
-  exo_compute(dummy_inputs, input_lens, private_key, 0);
+// we also have to check if decryption was valid
+// we make sure the first 16 bits are 0
+// this means that 2^48 is our max valid balance
+int valid_decryption(uint64_t x) {
+  int i;
+  int valid=1;
+  for (i=63; i>47; i--) {
+    if ((x >> i) & 1) {
+      valid=0;
+    }
+  }
+  return valid;
+}
 
-  int d = private_key[0];
-  int n = private_key[1];
+
+int compute(struct In *input, struct Out *output) {
+  // get witness values (private to prover)
+  uint64_t private_key[2];
+
+  uint64_t exo0_inputs1[1] = {1};
+  uint64_t *exo0_inputs_array[1] = {exo0_inputs1};
+  uint64_t exo0_lens[1] = {1};
+
+  exo_compute(exo0_inputs_array, exo0_lens, private_key, 0);
 
   // decrypt balance
-	uint32_t decrypted_balance = powm(input->encrypted_balance, d, n);
+  uint64_t decrypted_balance = powm(input->encrypted_balance, private_key[0], private_key[1]);
+  output->decrypted_balance = decrypted_balance;
+
+  // check validity
+  output->valid = valid_decryption(decrypted_balance);
 
   // test decrypted balance
-	output->valid = (decrypted_balance >= input->balance_to_prove);
-	return output->valid;
+  return (output->decrypted_balance >= input->balance_to_prove);
 }
 
 
