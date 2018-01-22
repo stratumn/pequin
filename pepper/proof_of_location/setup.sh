5eafe5b811f658a4fba747aed91fea3c0b96dea9
t-bast <bastien@stratumn.com>
2018-01-22 11:36:47 +0100

    proof of location: docker image

--- pepper/proof_of_location/setup.sh
+++ pepper/proof_of_location/setup.sh
@@ -8,8 +8,16 @@ APP=proof_of_location
 cp $APP/$APP.c apps/
 
 # Compile prover and verifier.
+echo Compiling proof of location prover and verifier...
 ./pepper_compile_and_setup_V.sh proof_of_location proof_of_location.vkey proof_of_location.pkey
 ./pepper_compile_and_setup_P.sh proof_of_location
 
+# Docker image.
+echo Building docker image...
+docker build -f proof_of_location/Dockerfile -t indigo/proof-of-location:latest .
+
 # Clean up.
+echo Cleaning up...
 rm apps/$APP.c
+
+echo Done!
